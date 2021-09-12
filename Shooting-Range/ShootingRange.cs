aae1cdcb16333ea53fdd9a489fb34f679cbb0976
rayzer <82989402+rayzerbrain@users.noreply.github.com>
2021-09-12 12:22:36 -0500

    Add files via upload

---
+++ Shooting-Range/ShootingRange.cs
@@ -0,0 +1,43 @@
+﻿using Exiled.API.Features;
+using Exiled.Events;
+using Exiled;
+using Exiled.API.Enums;
+using Player = Exiled.Events.Handlers.Player;
+using Server = Exiled.Events.Handlers.Server;
+using ShootingRange.EventHandlers;
+
+namespace ShootingRange
+{
+    public class ShootingRange : Plugin<Config>
+    {
+        private static readonly ShootingRange Singleton = new ShootingRange();
+        public static ShootingRange Instance => Singleton;
+        private ShootingRange()
+        {
+        }
+
+        public EventHandlers.EventHandlers EventHandler;
+
+        public override PluginPriority Priority { get; } = PluginPriority.High;
+
+        public override void OnEnabled()
+        {
+            EventHandler = new EventHandlers.EventHandlers();
+            Player.Joined += EventHandler.OnJoined;
+            Player.Died += EventHandler.OnDied;
+            Player.Shooting += EventHandler.OnShooting;
+            Server.RoundStarted += EventHandler.OnRoundStarted;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            EventHandler = null;
+            Player.Joined -= EventHandler.OnJoined;
+            Player.Died -= EventHandler.OnDied;
+            Player.Shooting -= EventHandler.OnShooting;
+            Server.RoundStarted -= EventHandler.OnRoundStarted;
+
+            base.OnDisabled();
+        }
+    }
+}
