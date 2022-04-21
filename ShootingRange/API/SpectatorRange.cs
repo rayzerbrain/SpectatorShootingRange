75ed30364c4ab6b3fea0fd46ccbd40f23fddddf7
rayzer <rayzerbrain@gmail.com>
2022-04-20 23:07:15 -0500

    g

--- ShootingRange/API/SpectatorRange.cs
+++ ShootingRange/API/SpectatorRange.cs
@@ -9,15 +9,13 @@ using Exiled.API.Features;
 using Exiled.API.Features.Toys;
 using Exiled.API.Extensions;
 
-using Object = UnityEngine.Object;
-
 namespace ShootingRange.API
 {
     public class SpectatorRange
     {
-        private BoxCollider _collider;
-        private Vector3 _smallBound = new Vector3(202, 970, -54);
-        private Vector3 _bigBound = new Vector3(237, 1000, -28);
+        private Primitive[] _walls = new Primitive[2];
+        private Vector3 _smallBound = new Vector3(202, 990, -54);
+        private Vector3 _bigBound = new Vector3(237, 1005, -35.4f);
         public Vector3 Spawn { get; } = new Vector3(218.5f, 999.1f, -43.0f);
         public bool IsOpen => Round.IsStarted && Respawn.TimeUntilRespawn > 20;
         public SpectatorRange() { }
@@ -65,16 +63,16 @@ namespace ShootingRange.API
             const int startX = 235;
             const int startZ = -53;
             const float y = 996.63f;
-            Vector3 rot = new Vector3(0, 1, 0);
+            Vector3 rot = new Vector3(0, 90, 0);
 
             for (int i = 0; i < 3; i++)
             {
-                float xOffset = dx * i;
+                float xOffset = dx * (i + 1);
                 float z = startZ - absZOffset - relZOffset * i;
 
                 ShootingTargetToy.Create(ShootingTargetType.Sport, new Vector3(startX - xOffset, y, z), rot);
-                ShootingTargetToy.Create(ShootingTargetType.ClassD, new Vector3(startX - xOffset * 3, y, z), rot);
-                ShootingTargetToy.Create(ShootingTargetType.Binary, new Vector3(startX - xOffset * 6, y, z), rot);
+                ShootingTargetToy.Create(ShootingTargetType.ClassD, new Vector3(startX - xOffset - 10, y, z), rot);
+                ShootingTargetToy.Create(ShootingTargetType.Binary, new Vector3(startX - xOffset - 20, y, z), rot);
             }
 
             //0 rotation = towards gate a
@@ -83,14 +81,22 @@ namespace ShootingRange.API
             //x smaller as going to A
             //z bigger going to escape
         }
-        public void SpawnCollider()
+        public void SpawnPrimitives()
         {
-            _collider = new BoxCollider()
+            //small z has no wall
+            //Primitive.Create((_smallBound + _bigBound) / 2, null, _bigBound - _smallBound, PluginMain.Instance.Config.UseCollider);
+            Vector3 dif = _bigBound - _smallBound;
+            Vector3 center = (_smallBound + _bigBound) / 2;
+            const float thickness = 0.1f;
+
+            _walls[0] = Primitive.Create(new Vector3(_bigBound.x, center.y, center.z), null, new Vector3(thickness, dif.y, dif.z));
+            _walls[1] = Primitive.Create(new Vector3(center.x, center.y, _bigBound.z), null, new Vector3(dif.x, dif.y, thickness));
+            foreach(Primitive wall in _walls)
             {
-                center = (_smallBound + _bigBound) / 2,
-                size = _bigBound - _smallBound
-            };
-            Object.Instantiate(_collider);
+                wall.Color = Color.clear;
+                wall.Type = PrimitiveType.Cube;
+                wall.Base.NetworkScale = wall.Base.transform.localScale;
+            }
         }
         //public void UnspawnCollider() => Object.Destroy(_collider);
         public void RemovePlayers()
